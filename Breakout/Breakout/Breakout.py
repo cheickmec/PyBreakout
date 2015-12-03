11ec911dbad690934ae4c75890549462ae844cad
Cheick <cab25004@vt.edu>
2015-12-03 13:53:57 -0500

    moving bird image

--- Breakout/Breakout/Breakout.py
+++ Breakout/Breakout/Breakout.py
@@ -1,16 +1,31 @@
 ﻿import pygame
-
-#screen size ocnstant
+############################ BREAKOUT ##############################################
+#####Screen size ocnstant
 SCREEN_SIZE = 640 , 480
-#game object
+####################################################################################
+#####Game object
 class Game(object):
+   
     def main(self,screen):
+        image = pygame.image.load('bird.png')
+        image_x = 320
+        image_y = 240
+
+        clock = pygame.time.Clock()
         while 1:
+            clock.tick(30)
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     return
                 if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                     return
+            image_x += 10
+            screen.fill((200,200,200))
+            screen.blit(image,(320,240))
+            screen.blit(image,(image_x,image_y))
+            pygame.display.flip()
+##################################################################################
 if __name__ == '__main__':
     #initialize pygame
     pygame.init()
