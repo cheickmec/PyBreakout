01fcfa7d1e3497e72a4566c9b046d29b3a9c9b83
Cheick <cab25004@vt.edu>
2015-12-03 13:21:05 -0500

    game class created

--- Breakout/Breakout/Breakout.py
+++ Breakout/Breakout/Breakout.py
@@ -1,4 +1,19 @@
 ﻿import pygame
+
+#screen size ocnstant
 SCREEN_SIZE = 640 , 480
-pygame.init()
-screen = pygame.display.set_mode(SCREEN_SIZE)
+#game object
+class Game(object):
+    def main(self,screen):
+        while 1:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                    return
+if __name__ == '__main__':
+    #initialize pygame
+    pygame.init()
+    #display screen
+    screen = pygame.display.set_mode(SCREEN_SIZE)
+    Game().main(screen)
